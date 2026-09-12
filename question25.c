3191c417dfa60a76e99a240d7a02b93bd3f6b529
Sanuj08 <152588291+Sanuj08@users.noreply.github.com>
2026-09-12 22:33:47 +0530

    Add files via upload

---
+++ question25.c
@@ -0,0 +1,49 @@
+//Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+/*
+Sample Test Cases:
+Input 1:
+4 2 +
+Output 1:
+6
+
+Input 2:
+10 3 %
+Output 2:
+1
+
+Input 3:
+15 5 /
+Output 3:
+3
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int a,b;
+    char operator;
+    printf("Enter two numbers and an operator (+, -, *, /, %): ");
+    scanf("%d %d %c", &a,&b,&operator);
+    switch(operator){
+    case'+':
+    printf("Result: %d\n", a+b);
+    break;
+    case'-':
+    printf("Result: %d\n", a-b);
+    break;
+    case'*':
+    printf("Result: %d\n", a*b);
+    break;
+    case'/':
+    if(b!=0)
+    {
+        printf("Result: %d\n",a/b);
+    }
+    else{
+        printf("Error: Division by zero");
+    }
+    return 0;
+}
+}
